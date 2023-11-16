349b84a76d714d43b797a2f087b0a4875c5177ca
Gino Scheuermann <scheuermann_gino@teams.hs-ludwigsburg.de>
2023-11-16 09:50:34 +0100

    Anpassungen

--- ersterAnsatz.py
+++ ersterAnsatz.py
@@ -2,8 +2,7 @@ import json
 
 class Zeiterfassung:
     alleZeiterfassungen = set()
-    def __init__(self, userId):
-        self.__userId = userId
+    def __init__(self):
         self.zeiten = dict()
         """
         self.__zeiten = {
@@ -18,14 +17,12 @@ class Zeiterfassung:
 
 class Nutzer:
     alleNutzer = set()
-    def __init__(self, rolle, nutzerId, mail, username, password):
+    def __init__(self, rolle, mail, password):
         self.__rolle = rolle
-        self.__nutzerId = nutzerId
         self.__mail = mail
-        self.__username = username
         self.__password = password
         self.__sperrung = False
-        self.zeiterfassung = Zeiterfassung(self.__nutzerId)
+        self.zeiterfassung = Zeiterfassung()
         Nutzer.alleNutzer.add(self)
 
     def sperreKonto(self):
@@ -35,26 +32,24 @@ class Nutzer:
     def neueBuchung(self, tag, stunden, bericht):
         if tag not in self.zeiterfassung.zeiten:                # prüft ob an dem Tag schon gearbeitet wurde, falls nicht:
             self.zeiterfassung.zeiten[tag] = dict()             # erstelle ein neuen Key im Dict und füge ihm als Wert ein Dict hinzu
-        self.zeiterfassung.zeiten[tag][stunden] = bericht       # gehe zum Key (Tag) und füge dem Tag die Stunden als Key und den Bericht als Value hinzu
+        self.zeiterfassung.zeiten[tag][bericht] = stunden       # gehe zum Key (Tag) und füge dem Tag die Stunden als Key und den Bericht als Value hinzu
 
     def speichereNutzerInDieJsonDatei():
         nutzer_liste = []
         for nutzer in Nutzer.alleNutzer:
             nutzer_info = {
             "Rolle": nutzer._Nutzer__rolle,
-            "NutzerId": nutzer._Nutzer__nutzerId,
             "Mail": nutzer._Nutzer__mail,
-            "Username": nutzer._Nutzer__username,
             "Password": nutzer._Nutzer__password,
             "Sperrung": nutzer._Nutzer__sperrung,
-            "Zeiterfassung": nutzer.zeiterfassung.zeiten         # hier wird nur die Zeiterfassung gespeichert, nicht das gesamte Objekt
+            "Zeiterfassung": nutzer.zeiterfassung.zeiten         # hier wird nur die Zeiterfassung gespeichert, nicht das gesamte Objekt (evtl problematisch!)
             }
             nutzer_liste.append(nutzer_info)
         with open("nutzerDatenbank.json", "w") as datei:
             json.dump(nutzer_liste, datei, indent=2)
 
-tomMustermann = Nutzer("einfache Anwender", 1001, "tom.mustermann@test.de", "tomM", "pw123")
-laraMeier = Nutzer("VIP Anwender", 1002, "lara.meier@test.de", "laraM", "pw456")
+tomMustermann = Nutzer("einfache Anwender", "tom.mustermann@test.de", "pw123")
+laraMeier = Nutzer("VIP Anwender", "lara.meier@test.de", "pw456")
 
 tomMustermann.neueBuchung("15.11.2023", "1 Stunde", "Wohnung geputzt")
 laraMeier.neueBuchung("10.11.2023", "3 Stunden", "Essen gekocht")
